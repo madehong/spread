8ef2f6cebf1dd6efdbd0d01a57532ec573dc7d65
madehong <madehong1992@163.com>
2016-11-29 21:46:05 +0800

    m

--- src/superclass.py
+++ src/superclass.py
@@ -25,33 +25,3 @@ class SuperLayer(object):
     def dump(self, path):
         with open(path + self.name + '.params', 'wb') as f:
             cPickle.dump(self.params, f)
-'''
-class SuperModel(object):
-    def __init__(self, name):
-        self.name = name
-        self.cost = 0.
-        self.layers = []
-    def train_model(self):
-        #Overwrite this method that includes you training codes in subclass!
-        pass
-    def valid_model(self):
-        #Overwrite this method that includes you validing codes in subclass!
-        pass
-    def test_model(self):
-        #Overwrite this method that includes you testing  codes in subclass!
-        pass
-    def get_updates(self, l1_norm=0., l2_norm=1e-5, optimizer=adagrad):
-        params = []
-        for layer in self.layers:
-            params += list(layer.params.values())
-        l1_norm = numpy.float32(l1_norm)
-        l2_norm = numpy.float32(l2_norm)
-        for param in params:
-            if l1_norm > 0.:
-                self.cost += T.sum(l1_norm * T.abs_(param), acc_dtype='float32')
-            if l2_norm > 0.:
-                self.cost += T.sum(l2_norm * T.sqrt(param), acc_dtype='float32')
-        gparams = [T.grad(self.cost, param) for param in params]
-        updates = adagrad(params, gparams)
-        return updates
-'''
